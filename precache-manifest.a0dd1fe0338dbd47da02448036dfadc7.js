324b40a677586e95bd1482f19c83cb0e145a894e
Bruno Capalbo <bruno.capalbo@fastalelo.onmicrosoft.com>
2020-01-28 11:23:18 -0300

    Updates

---
+++ precache-manifest.a0dd1fe0338dbd47da02448036dfadc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c00214a16cc823d5232d9ec8a9b13c2",
+    "url": "/qr-code/index.html"
+  },
+  {
+    "revision": "cf164e6e84fcce61aab6",
+    "url": "/qr-code/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fb15d93ce9fd89d15aab",
+    "url": "/qr-code/static/js/2.0890e81b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/qr-code/static/js/2.0890e81b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cf164e6e84fcce61aab6",
+    "url": "/qr-code/static/js/main.229ddd00.chunk.js"
+  },
+  {
+    "revision": "f1ba8136fe53f53cf238",
+    "url": "/qr-code/static/js/runtime-main.a160b917.js"
+  }
+]);
