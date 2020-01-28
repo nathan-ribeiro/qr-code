7d5aada44fa58557b5cec3a58396f0d254516a40
denerfonteszup <denerfonteszup>
2020-01-28 10:25:15 -0300

    leitor de qr code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import QrReader from 'react-qr-reader'
 import './App.css';
 
-function App() {
+const App = () => {
+  const [result, setResult] = useState('No Result')
+
+  const handleError = () => {
+    console.log('error')
+  }
+  
+  const handleScan = data => {
+    if(data) {
+      setResult(data)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>
+          <QrReader
+            delay={1}
+            onError={handleError}
+            onScan={handleScan}
+            style={{ width: '100%' }}
+          />
+          <p>{result}</p>
+        </div>
     </div>
   );
 }
